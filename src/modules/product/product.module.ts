c64564102553102caba1bcdb28db0a4a079d48e7
Safaa-Osama <safaaosama386@gmail.com>
2026-07-03 20:23:07 +0300

    add s3-service

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { ProductRepo } from 'src/common/reposetories/product-repo';
+import { CategoryRepo } from 'src/common/reposetories/category-repo';
+import { SubCategoryRepo } from 'src/common/reposetories/subCategory-repo';
+import { BrandRepo } from 'src/common/reposetories/brand-repo';
+import productModel from './entities/product.entity';
+import categoryModel from '../category/entities/category.entity';
+import subCategoryModel from '../sub-category/entities/sub-category.entity';
+import { brandModel } from '../brand/entities/brand.entity';
 
 @Module({
+  imports: [
+    productModel,
+    categoryModel,
+    subCategoryModel,
+    brandModel,
+  ],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [
+    ProductService,
+    ProductRepo,
+    CategoryRepo,
+    BrandRepo,
+    SubCategoryRepo
+  ],
 })
-export class ProductModule {}
+export class ProductModule { }
